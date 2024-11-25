8b63a83f1f8d03942d9875153438ac69aff5d5ba
Camilo Gomez <elcanserbero450@gmail.com>
2024-11-25 09:38:04 -0500

    Initial version of JhispterBackend generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/backendstyle/myapp/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.backendstyle.myapp.service;
---
+++ src/test/java/com/backendstyle/myapp/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.backendstyle.myapp;
+
+import com.backendstyle.myapp.JhispterBackendApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = JhispterBackendApp.class)
+public @interface IntegrationTest {
+}
